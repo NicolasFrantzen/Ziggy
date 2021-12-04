40f4346b8486adf4f950076d0a4934035c76cb20
Nicolas Bru Frantzen <79478528+NicolasBruFrantzen@users.noreply.github.com>
2021-12-04 12:11:47 +0100

    Add blockchain (#1)
    * Added blockchain module.
    
    * Added transactions.
    
    * Added hash function.
    
    * Store pending transactions on the blockchain.
    
    * Added proof of work.
    
    * Added grpc and protobuf to build.
    
    * Implemented some of the server communication layer.
    
    * Added mining.
    
    * Added Kanban board.
    
    * Added mining binary.
    
    * Implement previous_hash in mine response.
    
    * Add transactions from grpc call.

---
+++ src/blockchain.rs
@@ -0,0 +1,160 @@
+use std::mem;
+use std::time::{SystemTime};
+use sha2::{Sha256, Digest};
+//use hex_literal::hex;
+
+#[derive(Clone, Debug)]
+pub struct Block
+{
+    index: u64,
+    epoch: SystemTime,
+    proof: u64,
+    previous_hash: Sha256,
+    transactions: Vec<Transaction>,
+}
+
+
+impl Block
+{
+    pub fn get_index(&self) -> u64
+    {
+        self.index
+    }
+
+    pub fn get_time(&self) -> u128
+    {
+        self.epoch.duration_since(SystemTime::UNIX_EPOCH).expect("").as_millis()
+    }
+
+    pub fn get_proof(&self) -> u64
+    {
+        self.proof
+    }
+
+    pub fn get_previous_hash(&self) -> String
+    {
+        let digested = self.previous_hash.clone().finalize();
+
+        format!("{:X}", digested)
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Transaction
+{
+    sender: String,
+    recipient: String,
+    amount: f64,
+}
+
+
+pub struct Blockchain {
+    chain: Vec<Block>,
+    pending_transactions: Vec<Transaction>,
+}
+
+
+impl Blockchain
+{
+    pub fn new() -> Blockchain {
+        let mut new_chain = Vec::new();
+        new_chain.push(
+            Block {
+                index: 0,
+                epoch: SystemTime::now(),
+                proof: 0,
+                previous_hash: Sha256::new(),
+                transactions: Vec::new(),
+            }
+        );
+
+        Blockchain {
+            chain: new_chain,
+            pending_transactions: Vec::new(),
+        }
+    }
+
+
+    // TODO should probably return something smaller
+    // Cloning is very expensive
+    pub fn create_block(&mut self, proof: u64, previous_hash: Sha256) -> Block
+    {
+        let transactions = mem::take(&mut self.pending_transactions);
+        assert!(self.pending_transactions.is_empty());
+
+        let new_block: Block = Block{
+            index: (self.chain.len() + 1) as u64,
+            epoch: SystemTime::now(),
+            proof: proof,
+            previous_hash: previous_hash,
+            transactions: transactions,
+        };
+
+        self.chain.push(new_block.clone());
+
+        new_block
+    }
+
+
+    pub fn new_transaction(&mut self, sender: String, recipient: String, amount: f64)
+    {
+        self.pending_transactions.push(
+            Transaction{
+                sender: sender,
+                recipient: recipient,
+                amount: amount
+            }
+        )
+    }
+
+
+    pub fn get_last_block(&mut self) -> &Block
+    {
+        self.chain.last().expect("No elements in blockchain.")
+    }
+
+
+    pub fn hash(&mut self) -> Sha256
+    {
+        let block = self.get_last_block();
+
+        let mut hash = Sha256::new();
+        hash.update(block.index.to_le_bytes());
+        hash.update(block.get_time().to_le_bytes());
+        hash.update(block.proof.to_le_bytes());
+        //hash.update(self.get_last_block().previous_hash.finalize());
+
+        hash
+    }
+
+
+    pub fn proof_of_work(last_proof: u64) -> u64
+    {
+        let mut proof = 0;
+        loop
+        {
+            if Blockchain::validate_proof(last_proof, proof)
+            {
+                return proof
+            }
+            proof += 1;
+        }
+    }
+
+
+    pub fn validate_proof(last_proof: u64, proof: u64) -> bool
+    {
+        let mut hash = Sha256::new();
+        hash.update(last_proof.to_le_bytes());
+        hash.update(proof.to_le_bytes());
+        let digested = hash.finalize();
+
+        // TODO remove
+        {
+            let test: String = format!("{:X}", digested);
+            println!("Validating: {}", test);
+        }
+
+        digested[digested.len() - 2..] == [0x00, 0x00]
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,91 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Mutex;
+
+use tonic::{transport::Server, Request, Response, Status};
+
+use zigzag::ziggy_blockchain_server::{ZiggyBlockchain, ZiggyBlockchainServer};
+use zigzag::{MineResponse, NewTransactionRequest, NewTransactionResponse};
+mod zigzag;
+
+use blockchain::{Blockchain, Block};
+mod blockchain;
+
+
+struct MyZiggyBlockchain
+{
+    blockchain: Mutex<Blockchain>
+}
+
+
+impl MyZiggyBlockchain
+{
+    pub fn new() -> MyZiggyBlockchain
+    {
+        MyZiggyBlockchain { blockchain: Mutex::new(Blockchain::new()) }
+    }
+
+    fn mine_new_block(&self) -> Block
+    {
+        let mut chain = self.blockchain.lock().unwrap();
+        let last_block = chain.get_last_block();
+        let proof = Blockchain::proof_of_work(last_block.get_proof());
+        let hash = chain.hash();
+
+        // TODO: maybe use get_last_block ref instead
+        chain.create_block(proof, hash)
+    }
+
+    fn add_new_transaction(&self, sender: String, recipient: String, amount: f64)
+    {
+        let mut chain = self.blockchain.lock().unwrap();
+        chain.new_transaction(sender, recipient, amount)
+    }
+}
+
+
+#[tonic::async_trait]
+impl ZiggyBlockchain for MyZiggyBlockchain
+{
+    async fn mine(&self, _request: Request<()>) -> Result<Response<MineResponse>, Status>
+    {
+        println!("Mining request received.");
+
+        let new_block = self.mine_new_block();
+
+        dbg!(&new_block);
+
+        Ok(Response::new(MineResponse{
+             index: new_block.get_index(),
+             time: new_block.get_time() as u64,
+             proof: new_block.get_proof(),
+             previous_hash: new_block.get_previous_hash(),
+        }))
+    }
+
+    async fn new_transaction(&self, request: Request<NewTransactionRequest>) -> Result<Response<NewTransactionResponse>, Status>
+    {
+        println!("New transaction request received.");
+
+        let request = request.get_ref();
+
+        dbg!(request);
+
+        self.add_new_transaction(request.sender.clone(), request.recipient.clone(), request.amount);
+
+        Ok(Response::new(NewTransactionResponse{}))
+    }
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>
+{
+    let addr = "[::1]:50051".parse().unwrap();
+    let service = MyZiggyBlockchain::new();
+    println!("Server listening on {}", addr);
+
+    Server::builder()
+        .add_service(ZiggyBlockchainServer::new(service))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
