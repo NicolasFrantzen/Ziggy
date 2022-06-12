2e2ac76d875d86a0bd1b3ddd5c4d52086f2e35e9
Nicolas Bru Frantzen <nicolas.bru.frantzen@gmail.com>
2022-06-12 17:31:21 +0200

    Implement register node(s).

--- src/bin/consensus.rs
+++ src/bin/consensus.rs
@@ -1,13 +1,53 @@
 use clap::{Arg, App, AppSettings};
-use anyhow::{Result, anyhow};
+use anyhow::Result;
 
+use tonic::transport::Channel;
 use ziggy::zigzag;
+use zigzag::RegisterNodesRequest;
+use zigzag::register_nodes_request::Node as GrpcNode;
 use zigzag::ziggy_service_client::ZiggyServiceClient;
 
 
-async fn register_node()
+async fn create_client() -> Result<ZiggyServiceClient<Channel>>
 {
+    let channel = tonic::transport::Channel::from_static("http://[::1]:50051")
+        .connect()
+        .await?;
 
+    Ok(ZiggyServiceClient::new(channel))
+}
+
+
+/// Register one node
+async fn register_node(address: &str, port: u32) -> Result<()>
+{
+    let mut client = create_client().await?;
+
+    let new_node = GrpcNode {
+        address: String::from(address),
+        port
+    };
+    let response = client.register_nodes(tonic::Request::new(
+        RegisterNodesRequest {
+            nodes: vec![new_node],
+         }
+    )).await?.into_inner();
+
+    dbg!(response);
+
+    Ok(())
+}
+
+
+/// Get full chain
+async fn get_chain() -> Result<()>
+{
+    let mut client = create_client().await?;
+
+    let response = client.get_chain(tonic::Request::new(())).await?.into_inner();
+    dbg!(response);
+
+    Ok(())
 }
 
 
@@ -15,6 +55,7 @@ async fn register_node()
 async fn main() -> Result<()>
 {
     let args = App::new("consensus")
+        .subcommand(App::new("getchain"))
         .subcommand(App::new("register")
             .arg(Arg::new("address")
                 .help("Address of the node to register")
@@ -27,24 +68,18 @@ async fn main() -> Result<()>
         .setting(AppSettings::ArgRequiredElseHelp)
         .get_matches();
 
-    match args.subcommand_name()
+    match args.subcommand()
     {
-        Some(args) => match args {
-            "register" => println!("Register not implemented yet"),
-            _ => ()
-        },
-        None => (),
+        Some(("getchain", _)) =>
+            get_chain().await?,
+
+        Some(("register", sub_matches)) =>
+            register_node(
+                sub_matches.value_of("address").unwrap(), // TODO: validate
+                sub_matches.value_of_t("port").unwrap_or(50051), // TODO: validate
+            ).await?,
+        _ => (),
     }
-
-
-    let channel = tonic::transport::Channel::from_static("http://[::1]:50051")
-        .connect()
-        .await?;
-
-    let mut client = ZiggyServiceClient::new(channel);
-
-    let response = client.get_chain(tonic::Request::new(())).await?.into_inner();
-    dbg!(response);
 
     Ok(())
 }
